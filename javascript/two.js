a7ee92868406aabb4d01fc4666835c778a01ae69
PrasannaKV1 <prasannapooja215@gmail.com>
2025-05-28 13:12:18 +0530

    this is the commit message

---
+++ javascript/two.js
@@ -0,0 +1,8 @@
+let num=20;
+console.log(num); 
+console.log(typeof num); // number
+console.log(num.toString()); // "20"
+console.log(typeof num.toString()); // string
+let str ='code threaded';
+console.log(str);   
+console.log(typeof str); // string
